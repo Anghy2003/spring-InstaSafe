dd60426fe2fcd282ca9b6811ac169c3fd35e8a64
AndresBerre23 <andres.berrezueta.est@tecazuay.edu.ec>
2025-07-10 09:53:50 -0500

    importantisimo

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,4 +3,6 @@ insert into roles(nombre) values ('Guardia');
 insert into roles(nombre) values ('Docente');
 insert into roles(nombre) values ('Estudiante');
 
-INSERT INTO usuario (cedula,nombre,apellido,correo,foto,genero,fechanacimiento,contrasena,fecharegistro,id_rol) VALUES ('0106944937','Melo','Illescas','melito@gmail.com','https://drive.google.com/file/d/1kOX4LOiNTGTfELh4PxgpbszZJrwFLs4q/view?usp=sharing','Femenino',TO_TIMESTAMP('20010-15-10 19:00:00', 'YYYY-MM-DD HH24:MI:SS'),'abcd1234',TO_TIMESTAMP('2025-07-08 20:42:31', 'YYYY-MM-DD HH24:MI:SS'),1);
+
+INSERT INTO usuarios (cedula,nombre,apellido,correo,foto,genero,fechanacimiento,contrasena,id_rol) VALUES ('0106944945','Melo','Illescas','melito@gmail.com','https://drive.google.com/file/d/1kOX4LOiNTGTfELh4PxgpbszZJrwFLs4q/view?usp=sharing','Femenino','2010-10-15','abcd1234',1);
+INSERT INTO usuarios (cedula,nombre,apellido,correo,foto,plantilla_facial,genero,idresponsable,fechanacimiento,contrasena,id_rol) VALUES ('0106944937','Angie','Illescas','aillescas@gmail.com','https://eduv.tecazuay.edu.ec/pluginfile.php/40622/user/icon/academi/f1?rev=594406',NULL,'Femenino',NULL,'2003-12-23','Mayra22.',2);
